d66f224f2bd05d7a6641b80f35c53cd8886d23f8
kip2 <tqa1no4@gmail.com>
2023-09-10 15:44:54 +0900

    数字がリストにある場合はループして生成するようにしている

--- portNumberGenerator.py
+++ portNumberGenerator.py
@@ -8,10 +8,26 @@ def create_port_number() :
     """
     # 以下の範囲で番号を生成する
     # 1024~65535
-    r = random.randint(1024, 65535)
+    #rangeMin = 1024
+    #rangeMax = 65535
+    rangeMin = 1024
+    rangeMax = 1150 
+
+    r = random.randint(rangeMin, rangeMax)
+
+    while (r in port_number_arr):
+        r = random.randint(rangeMin, rangeMax)
+        #print("被った！", end="")
+
+    port_number_arr.append(r)
     return r
 
 if __name__ == "__main__":
-    print(create_port_number())
+    """
+        test case: 被った時にちゃんとループし直しているか
+    """
+    for i in range(100):
+        print(create_port_number())
+    print(port_number_arr)
 
     
