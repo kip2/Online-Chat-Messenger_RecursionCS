920481342f52c047a82c29ff6f0272bc296fb47d
kip2 <tqa1no4@gmail.com>
2023-09-12 21:38:43 +0900

    save: serverメッセージとmessage_log用の型枠だけ用意

---
+++ chat_message_log.py
@@ -0,0 +1,10 @@
+def json_load():
+    pass
+
+def display():
+    pass
+
+if __name__ == "__main__":
+    json_load()
+    display()
+    pass
---
+++ server_chat_message.py
@@ -0,0 +1,11 @@
+def json_load():
+    pass
+
+def save_message_to_ndjson():
+    pass
+
+if __name__ == "__main__":
+    response_message = json_load()
+    save_message_to_ndjson()
+
+    
