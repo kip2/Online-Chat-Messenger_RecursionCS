930570878fe4c89c7efedabbd4b70a63ec1019f4
kip2 <tqa1no4@gmail.com>
2023-09-17 19:30:24 +0900

    fix: headerの数値を更新

--- _header.py
+++ _header.py
@@ -1,11 +1,11 @@
 # const request header
-CREATE_ROOM = 0
-REQUEST_ROOM_LIST = 1
-REQUEST_LOG_FILE = 2
-ENTER_ROOM = 3
-SEND_MESSAGE = 4
-CLIENT_EXIT_MESSAGE = 5
-EXIT_MESSAGE = 9
+CREATE_ROOM = 1
+REQUEST_ROOM_LIST = 2
+REQUEST_LOG_FILE = 3
+ENTER_ROOM = 4
+SEND_MESSAGE = 5
+CLIENT_EXIT_MESSAGE = 6
+EXIT_MESSAGE = 0
 
 def create_header(client_request, message_length,data_length):
     return client_request.to_bytes(1, "big") + message_length.to_bytes(3,"big") + data_length.to_bytes(4, "big")
