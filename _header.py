65b1a76dcab2519b7d25caebb0cfe4de18b4fc16
kip2 <tqa1no4@gmail.com>
2023-09-17 18:26:37 +0900

    fix: create_header関数を外部ファイルに移動

--- _header.py
+++ _header.py
@@ -6,3 +6,6 @@ ENTER_ROOM = 3
 SEND_MESSAGE = 4
 CLIENT_EXIT_MESSAGE = 5
 EXIT_MESSAGE = 9
+
+def create_header(client_request, message_length,data_length):
+    return client_request.to_bytes(1, "big") + message_length.to_bytes(3,"big") + data_length.to_bytes(4, "big")
