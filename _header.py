c057c437ada0cb7aa8460d1eb38e6d60355a6a8a
kip2 <tqa1no4@gmail.com>
2023-09-17 21:06:26 +0900

    fix: headerにコメントを追加

--- _header.py
+++ _header.py
@@ -8,4 +8,8 @@ CLIENT_EXIT_MESSAGE = 6
 EXIT_MESSAGE = 0
 
 def create_header(client_request, message_length,data_length):
+    """
+        送信用のheaderを作成する関数
+    """
+    # todo: やりとりの様式が決まっていないので、仮で実装している
     return client_request.to_bytes(1, "big") + message_length.to_bytes(3,"big") + data_length.to_bytes(4, "big")
