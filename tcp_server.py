60ae97b4e98b8e8318aa2a9bfc44e47f1f030c0e
kip2 <tqa1no4@gmail.com>
2023-09-17 21:09:32 +0900

    fix: 若干のコード修正

--- tcp_server.py
+++ tcp_server.py
@@ -1,12 +1,7 @@
-import _address_config
 from port_scan import *
 
-# network socket type
-NETWORK_SOCKET_TYPE = _address_config.NETWORK_SOCKET_TYPE 
-# server address
-SERVER_ADDRESS = _address_config.SERVER_ADDRESS 
-# server port
-SERVER_PORT = _address_config.SERVER_PORT
+# 定数読み込み用
+import _address_config
 
 class TCP_Server:
     def __enter__(self):
@@ -16,7 +11,7 @@ class TCP_Server:
     def __exit__(self, *args):
         self.sock.close()
 
-def startup_tcp_server(server_address:str = SERVER_ADDRESS, server_port:int = None) -> tuple:
+def startup_tcp_server(server_address:str = _address_config.SERVER_ADDRESS, server_port:int = None) -> tuple:
     """
         TCPサーバーを立てる関数
         connect用のsocketと、(IP_address, PORT)のタプルを返す
@@ -26,7 +21,7 @@ def startup_tcp_server(server_address:str = SERVER_ADDRESS, server_port:int = No
         server_port = available_tcp_port(server_address)
 
     try:
-        sock = socket.socket(NETWORK_SOCKET_TYPE, socket.SOCK_STREAM) 
+        sock = socket.socket(_address_config.NETWORK_SOCKET_TYPE, socket.SOCK_STREAM) 
         sock.bind((server_address, server_port))
         sock.listen(1)
         return (sock, server_address, server_port)
--- udp_server.py
+++ udp_server.py
@@ -1,14 +1,6 @@
 import _address_config
 from port_scan import *
 
-# network socket type
-NETWORK_SOCKET_TYPE = _address_config.NETWORK_SOCKET_TYPE 
-
-# server address
-SERVER_ADDRESS = _address_config.SERVER_ADDRESS 
-# server port
-SERVER_PORT = _address_config.SERVER_PORT
-
 class UDP_Server:
     def __enter__(self):
         self.sock, self.addr, self.port = startup_udp_server()
@@ -18,7 +10,7 @@ class UDP_Server:
         self.sock.close()
 
 
-def startup_udp_server(server_address:str = SERVER_ADDRESS, server_port:str = None) -> tuple:
+def startup_udp_server(server_address:str = _address_config.SERVER_ADDRESS, server_port:str = None) -> tuple:
     """
         UDPサーバーを立てる関数
         connect用のsocketと、(IP_address, PORT)のタプルを返す
@@ -28,7 +20,7 @@ def startup_udp_server(server_address:str = SERVER_ADDRESS, server_port:str = No
         server_port = available_udp_port(server_address)
 
     try:
-        sock = socket.socket(NETWORK_SOCKET_TYPE, socket.SOCK_DGRAM) 
+        sock = socket.socket(_address_config.NETWORK_SOCKET_TYPE, socket.SOCK_DGRAM) 
         sock.bind((server_address, server_port))
         return (sock, server_address, server_port)
     except (socket.timeout, ConnectionRefusedError):
