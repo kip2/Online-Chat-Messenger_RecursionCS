73d6f064ef796561be8efc15d966baf6d7197b44
kip2 <tqa1no4@gmail.com>
2023-09-25 20:22:15 +0900

    add: create json_server.py

---
+++ json_server.py
@@ -0,0 +1,18 @@
+from lib.tcp_server import *
+
+import lib
+
+# network socket type
+NETWORK_SOCKET_TYPE = lib._address_config.NETWORK_SOCKET_TYPE 
+# server address
+SERVER_ADDRESS = lib._address_config.SERVER_ADDRESS 
+# server port
+SERVER_PORT = lib._address_config.SERVER_PORT
+
+def json_server():
+    pass
+
+if __name__ == "__main__":
+    print("Hello")
+    json_server()
+    pass
