400480fd19ef3d9d1324c1ebe1bcd9f11efc4fff
kip2 <tqa1no4@gmail.com>
2023-09-25 22:38:53 +0900

    fix: loop処理は無駄たったので削除した

--- json_server.py
+++ json_server.py
@@ -11,7 +11,7 @@ SERVER_ADDRESS = lib._address_config.SERVER_ADDRESS
 JSON_SERVER_PORT = lib._address_config.JSON_SERVER_PORT
 
 # json save directory
-JSON_DIRECTORY_PATH = "json"
+JSON_DIRECTORY_PATH = "temp"
 
 def send_json_server(filepath):
     pass
@@ -23,44 +23,41 @@ def recieve_json_server():
         # create json directory
         create_json_directory()
 
-        while True:
-            connection, client_address = sock.accept()
-            try:
-                print("connection from", client_address)
-                header = connection.recv(8)
+        connection, client_address = sock.accept()
+        try:
+            print("connection from", client_address)
+            header = connection.recv(8)
 
-                filename_length = int.from_bytes(header[:1], "big")
-                json_length = int.from_bytes(header[1:3], "big")
-                data_length = int.from_bytes(header[4:8], "big")
-                stream_rate = 4096
+            filename_length = int.from_bytes(header[:1], "big")
+            json_length = int.from_bytes(header[1:3], "big")
+            data_length = int.from_bytes(header[4:8], "big")
+            stream_rate = 4096
 
-                # todo: 確認終われば消す
-                print(f'Recieved header from client. Byte lengths: Title length {filename_length}, JSON length {json_length}, Data Length {data_length}')
+            # todo: 確認終われば消す
+            print(f'Recieved header from client. Byte lengths: Title length {filename_length}, JSON length {json_length}, Data Length {data_length}')
 
-                # todo: encodeのCHARACTER CODEを定数にする
-                filename = connection.recv(filename_length).decode("utf-8")
-                # todo: 確認終われば消す
-                print(f"Filename: {filename}")
+            # todo: encodeのCHARACTER CODEを定数にする
+            filename = connection.recv(filename_length).decode("utf-8")
+            # todo: 確認終われば消す
+            print(f"Filename: {filename}")
 
-                if json_length != 0:
-                    raise Exception("JSON data is not currently supported.")
-                if data_length == 0:
-                    raise Exception("No data to read from client.")
+            if json_length != 0:
+                raise Exception("JSON data is not currently supported.")
+            if data_length == 0:
+                raise Exception("No data to read from client.")
 
-                with open(os.path.join(JSON_DIRECTORY_PATH, filename), "wb+") as f:
-                    while data_length > 0:
-                        data = connection.recv(data_length if data_length <= stream_rate else stream_rate)
-                        f.write(data)
-                        print(f"recieved {len(data)} bytes")
-                        data_length -= len(data)
-                        print(data_length)
+            with open(os.path.join(JSON_DIRECTORY_PATH, filename), "wb+") as f:
+                while data_length > 0:
+                    data = connection.recv(data_length if data_length <= stream_rate else stream_rate)
+                    f.write(data)
+                    print(f"recieved {len(data)} bytes")
+                    data_length -= len(data)
+                    print(data_length)
 
-                print("Finished downloading the file from client.")
-                # 一回ごとに通信を終わること
-                break
-                
-            except Exception as e:
-                print("Error: " + str(e))
+            print("Finished downloading the file from client.")
+            # 一回ごとに通信を終わること
+        except Exception as e:
+            print("Error: " + str(e))
         
 def create_json_directory():
     """
